b7262bc891b07d7cc23a371446d6871b3dc458fb
Bogdan Andrei <bogdan.andrei450@gmail.com>
2020-06-24 21:27:18 +0300

    add __init__py and models.py

---
+++ models.py
@@ -0,0 +1,42 @@
+from . import db
+
+
+class Client(db.Model):
+    __tablename__ = 'client'
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    prenume = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    nume = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    localitate = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    adresa_ip = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+    abonament = db.Column(
+        db.String(64),
+        index=False,
+        unique=True,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return '<Client {}>'.format(self.prenume)
