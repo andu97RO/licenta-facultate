a4ab83245d4f6eb7f0f5b35a49d007e238e00fb1
Bogdan Andrei <bogdan.andrei450@gmail.com>
2020-06-26 22:25:36 +0300

    add final files

--- ISP_Admin_Page/routes.py
+++ ISP_Admin_Page/routes.py
@@ -3,10 +3,10 @@ from datetime import datetime as dt
 from flask import current_app as app
 from flask import redirect, render_template, url_for, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
+from wtforms import StringField, SelectField, RadioField
 from wtforms.validators import InputRequired
 
-from .models import Client, db
+from .models import Client, Abonament, IP, db
 
 from flask_bootstrap import Bootstrap
 
@@ -28,6 +28,25 @@ class AdaugaClientForm(FlaskForm):
         '30 RON', '30 RON'), ('50 RON', '50 RON'), ('100 RON', '100 RON')])
 
 
+class AdaugaAbonamentForm(FlaskForm):
+    valoare = StringField('Valoare', validators=[
+        InputRequired(message='Trebuie introdusa o valoare in RONI!')])
+    vitezamin = StringField('VitezaMin', validators=[InputRequired(
+        message='Trebuie introudsa o viteza min (in kbps)!')])
+    vitezamax = StringField('VitezaMax', validators=[
+        InputRequired(message='Trebuie introdusa o viteza maxima (in kbps)!')])
+
+
+class AdaugaIPForm(FlaskForm):
+    adresa_ip = StringField('IP', validators=[
+        InputRequired(message='Trebuie introdusa o valoare in RONI!')])
+    nume = StringField('VitezaMin', validators=[InputRequired(
+        message='Trebuie introudsa o viteza min (in kbps)!')])
+    prenume = StringField('VitezaMax', validators=[
+                          InputRequired(message='Trebuie introdusa o viteza maxima (in kbps)!')])
+    activ = RadioField('Activeaza')
+
+
 @app.route('/clienti/<int:page>', methods=['GET'])
 def clienti(page=1):
     per_page = 4
@@ -37,6 +56,30 @@ def clienti(page=1):
         'clienti.html',
         clienti=clienti,
         title="Clienti",
+    )
+
+
+@app.route('/ip/<int:page>', methods=['GET'])
+def ip(page=1):
+    per_page = 4
+    ipuri = IP.query.paginate(page, per_page, error_out=False)
+
+    return render_template(
+        'ip.html',
+        ip=ipuri,
+        title="IP",
+    )
+
+
+@app.route('/abonament/<int:page>', methods=['GET'])
+def abonament(page=1):
+    per_page = 4
+    abonamente = Abonament.query.paginate(page, per_page, error_out=False)
+
+    return render_template(
+        'abonament.html',
+        abonament=abonamente,
+        title="Abonamente",
     )
 
 
@@ -50,7 +93,7 @@ def clienti_modifica(id_client):
 
         update_dict = {"prenume": form.prenume.data, "nume": form.nume.data,
                        "localitate": form.localitate.data,
-                       "adresa": form.adresa.data, "telefon": form.telefon.data, 
+                       "adresa": form.adresa.data, "telefon": form.telefon.data,
                        "abonament": form.abonament.data}
 
         db.session.query(Client).filter(
@@ -58,7 +101,28 @@ def clienti_modifica(id_client):
         db.session.commit()
         return redirect(url_for('clienti_arata', id_client=id_client))
 
-    return render_template('clientiedit.html', id_client=id_client, form=form, client=client)
+    return render_template('clientiedit.html', id_client=id_client, form=form,
+                           client=client)
+
+
+@app.route('/abonament/modifica/<id_abonament>', methods=['POST', 'GET'])
+def abonament_modifica(id_abonament):
+    form = AdaugaAbonamentForm()
+
+    abonament = Abonament.query.get(id_abonament)
+
+    if form.validate_on_submit():
+
+        update_dict = {"valoare": form.valoare.data, "vitezamin": form.vitezamin.data,
+                       "vitezamax": form.vitezamax.data}
+
+        db.session.query(Abonament).filter(
+            Abonament.id == id_abonament).update(update_dict)
+        db.session.commit()
+        return redirect(url_for('abonament_arata', id_abonament=id_abonament))
+
+    return render_template('abonamentedit.html', id_abonament=id_abonament, form=form,
+                           abonament=abonament)
 
 
 @app.route('/clienti/arata/<id_client>', methods=['GET'])
@@ -66,16 +130,35 @@ def clienti_arata(id_client):
 
     client = Client.query.get(id_client)
     print(client)
-    return render_template('clientishow.html', client=client, title="Arata client")
+    return render_template('clientishow.html', client=client,
+                           title="Arata client")
+
+
+@app.route('/abonament/arata/<id_abonament>', methods=['GET'])
+def abonament_arata(id_abonament):
+
+    abonament = Abonament.query.get(id_abonament)
+    return render_template('abonamentshow.html', abonament=abonament,
+                           title="Arata abonament")
 
 
 @app.route('/clienti/sterge/<id_client>', methods=['GET'])
 def clienti_sterge(id_client):
-    
     Client.query.filter(Client.id == id_client).delete()
     db.session.commit()
 
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return json.dumps({'success': True}), 200,
+    {'ContentType': 'application/json'}
+
+
+@app.route('/abonament/sterge/<id_abonament>', methods=['GET'])
+def abonament_sterge(id_abonament):
+    Abonament.query.filter(Abonament.id == id_abonament).delete()
+    db.session.commit()
+
+    return json.dumps({'success': True}), 200,
+    {'ContentType': 'application/json'}
+
 
 @app.route('/factura/<id_client>', methods=['GET'])
 def factura(id_client):
@@ -98,3 +181,52 @@ def clienti_adauga():
         return redirect(url_for('clienti', page=1))
 
     return render_template('clientiform.html', form=form)
+
+
+@app.route('/ip/adauga', methods=['POST', 'GET'])
+def ip_adauga():
+    form = AdaugaClientForm()
+
+    if form.validate_on_submit():
+        ip_nou = Client(adresa_ip=form.adresa_ip.data,
+                        nume=form.nume.data,
+                        prenume=form.prenume.data,
+                        activ=form.activ.data)
+        db.session.add(ip_nou)
+        db.session.commit()
+        return redirect(url_for('ip', page=1))
+
+    return render_template('ipform.html', form=form)
+
+
+@app.route('/abonament/adauga', methods=['POST', 'GET'])
+def abonament_adauga():
+    form = AdaugaAbonamentForm()
+
+    if form.validate_on_submit():
+        abonament_nou = Abonament(valoare=form.valoare.data,
+                                  vitezamin=form.vitezamin.data,
+                                  vitezamax=form.vitezamax.data)
+        db.session.add(abonament_nou)
+        db.session.commit()
+        return redirect(url_for('abonament', page=1))
+
+    return render_template('abonamentform.html', form=form)
+
+
+@app.route('/regula/adauga/<int:client_id>')
+def regula_adauga(client_id):
+    client = Client.query.filter(Client.id == client_id).first()
+    abonament = Abonament.query.filter(
+        Abonament.valoare == client.abonament).first()
+
+    # $downmax = $line5['value'];
+    # $upmax = floor($downmax / 2);
+
+    # // upload eno1
+    # $filename = '/etc/sysconfig/htb/eno1-2:'.($line["id"]+1);
+    # $continut = "RATE=".$line4['value']."kbit\nCEIL=".($upmax)."kbit\nMARK=".($line['id']+1)."\nPRIO=5\n";
+
+    # // download eno2
+    # $filename2 = '/etc/sysconfig/htb/eno2-2:'.($line["id"]+1);
+    # $continut2 = "RATE=".$line4['value']."kbit\nCEIL=".$downmax."kbit\nPRIO=5\n";
--- ISP_Admin_Page/static/js/app.js
+++ ISP_Admin_Page/static/js/app.js
@@ -3,9 +3,6 @@ console.log("We are live")
 $( ".client_sterge" ).on("click", function() {
     var client_id = $(this).attr('id')
     var returncode = confirm("Esti sigur ca vrei sa stergi clientul?")
-    
-    console.log("id ul este " + client_id)
-    console.log('aaa')
     if (returncode) {
         $.ajax({
             type: "GET",
@@ -14,6 +11,25 @@ $( ".client_sterge" ).on("click", function() {
             success: function(data, textStatus) {
                 if (data) {
                     window.location.href = '/clienti/1';
+                } else {
+                    console.log("OK")
+                }
+            }
+        })
+    }
+  })
+
+  $( ".abonament_sterge" ).on("click", function() {
+    var abonament_id = $(this).attr('id')
+    var returncode = confirm("Esti sigur ca vrei sa stergi abonamentul?")
+    if (returncode) {
+        $.ajax({
+            type: "GET",
+            url: '/abonament/sterge/' + abonament_id,
+            dataType: "json",
+            success: function(data, textStatus) {
+                if (data) {
+                    window.location.href = '/abonament/1';
                 } else {
                     console.log("OK")
                 }
